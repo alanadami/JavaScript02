093f277159feec43ea9c3fa5f5a265c9189d3e5d
alanadami <166352617+alanadami@users.noreply.github.com>
2024-05-10 02:36:08 -0300

    iu

---
+++ Curso_freeCodeCamp/index.js
@@ -0,0 +1,42 @@
+//document.getElementById("count").innerText = 5
+
+
+let count = 0
+function increment(){
+    console.log(1)
+}
+cont = count + increment
+console.log(count)
+
+
+/*
+let bonusPoint = 50
+bonusPoint = bonusPoint + 50
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint - 75
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint + 45
+console.log(bonusPoint)
+
+
+let myAge = 42
+let humamDogRatio = 7
+
+let myDogAge = myAge * humamDogRatio
+
+console.log(myDogAge)
+
+
+let count = 0
+console.log(count)
+
+let myAge = 43
+console.log(myAge)
+
+let primeirovagao = 5
+let segundovagao = 7
+let trem = primeirovagao+segundovagao
+console.log(trem)
+*/
