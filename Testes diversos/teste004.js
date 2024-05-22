aebb05c69c01d6c79418c91c4c1fa32e8e6ccc8d
alanadami <166352617+alanadami@users.noreply.github.com>
2024-05-22 01:10:10 -0300

    exercícios com js
    Saber se é par ou impar
    uso do for

--- Testes diversos/teste004.js
+++ Testes diversos/teste004.js
@@ -1,14 +1,84 @@
 
-const notas = [9.0, 7.0, 10.0]
 
-function parImpar(){
-    let media = 
+
+
+
+/*
+// Uso do for para montar uma tabuada (Não entendi)
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      const resultado = i * j
+      console.log(i + ' x ' + j + ' = ' + resultado)
+    }
+  }
+
+
+
+// Uso do For
+
+for (let i= 0; i< 10; i++) {
+    const esPar = i%2 ===0
+    if (esPar) {
+        continue
+    }
+    console.log(i)
+
+    if (i ===7){
+        break
+    }
 }
 
 
-//alert("Seja bem vindo!")
+    //valor inicial     se true, então contagem --
+for (let contagem = 10; contagem >=0; contagem --){
+    if (contagem === 0){
+        alert("Decolar!!")
+    } else {
+        alert("Ainda falta " + contagem + " Segundos!")
+    }
+}
 
-/*
+
+for (let numero = 1; numero <=10; numero ++) {
+    alert(numero)
+}
+
+
+
+// colocar um string em maiúsculo
+function parImpar(){
+    let valor = document.getElementById("itexto").value;
+    let palavra = valor
+    
+    alert(palavra.toUpperCase());     
+}
+
+
+
+var numeros = [1,2,3,4,5,6,7,8,9]
+var index = numeros.indexOf(1);
+
+console.log(index)
+
+
+
+const lista =["homem", "branco", "gremista", "olhos esverdiados"]
+
+lista.push("43 anos")
+lista.unshift("Professor")
+lista.splice(4, 0, "cantor")
+
+delete lista[3]
+
+alert(lista)
+console.log(lista)
+
+
+
+
+alert("Seja bem vindo!")
+
+
 function parImpar(){
     let numero = document.getElementById("inumero").value;
     let total = numero%2;
